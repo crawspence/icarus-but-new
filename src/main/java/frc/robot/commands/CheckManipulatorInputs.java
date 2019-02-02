9a5bfe926e78b2dacfef3beef99c9b7fbd070bfb
Remy Bacigalupi <remybaci@icloud.com>
2019-02-02 16:44:23 -0500

    except that shouldn't be there

--- src/main/java/frc/robot/commands/CheckManipulatorInputs.java
+++ src/main/java/frc/robot/commands/CheckManipulatorInputs.java
@@ -28,7 +28,6 @@ public class CheckManipulatorInputs extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    
   }
 
   // Called repeatedly when this Command is scheduled to run
